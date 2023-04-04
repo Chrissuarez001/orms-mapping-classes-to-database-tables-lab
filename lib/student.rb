857d7dea9cbb63767324031de85d2bf59afe35fc
Christopher Suarez <chrissuarez001@gmail.com>
2023-04-04 09:40:45 -0400

    commit tree fef10bc89b75c9421a2c18d366ae1b39cec5d2d0

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,43 @@
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+attr_accessor :name, :grade
+attr_reader  :id
   
+def initialize (name, grade, id=nil)
+  @id = id 
+  @name = name
+  @grade = grade
+end
+
+def self.create_table
+  sql =  <<-SQL 
+  CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+  )
+  SQL
+  DB[:conn].execute(sql)
+end
+end
+
+def self.drop_table
+  sql =  <<-SQL 
+  DROP TABLE IF EXISTS students
+  DB[:conn].execute(sql)
+end
+
+def save 
+  sql = <<-SQL
+  INSERT INTO students (name, grade)
+  VALUES (?, ?)
+  SQL
+  DB[:conn].execute(sql, self.name, self.grade)
+
+end 
+
+def self.create (name:, grade:)
+students = Student.new(name: , grade:)
+students.save
+students
+end
 end
